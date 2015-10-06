1981d6df4a5020fc990122e2abda7c7f7e713628
Sergio Areco <sergio@MacBook-Pro-de-Sergio.local>
2015-10-06 20:13:39 -0300

    config CORS

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ var bodyParser = require('body-parser');
 mongoose.connect('mongodb://admin:admin@ds037283.mongolab.com:37283/prodenserio');
 
 // Express
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'file:///Users/sergio/Google%20Drive/Projects/test.html');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
 var app = express();
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/api',require('./routes/api'));
+app.use(allowCrossDomain);
 
 // Start server
 //app.listen(3000);
