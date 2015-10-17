a9a81f54086383ac9bab00ea7f5635b7b6e14b51
Sergio Areco <sergio@MacBook-Pro-de-Sergio.local>
2015-10-17 01:36:49 -0300

    config server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://admin:admin@ds037283.mongolab.com:37283/prodenserio'
 //CORS middleware
 var app = express();
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "prodenserio.com.ar");
+  res.header("Access-Control-Allow-Origin", "http://www.prodenserio.com.ar");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
